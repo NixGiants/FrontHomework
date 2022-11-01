17e0e933eda8ef2becd4c2f738bcd3f8dee2dac8
Roman Riabenko <beatle.gaze@gmail.com>
2022-11-01 12:23:08 +0200

    Fix styles  for SignInPage

--- front-homework/src/pages/mainPage/SignInPage.jsx
+++ front-homework/src/pages/mainPage/SignInPage.jsx
@@ -8,24 +8,26 @@ function SignInPage() {
     return (
         <>
             <SignInHeader />
-            <div class="row justify-content-center align-items-center" >
-            <form class="sign-in">
-                <fieldset>
-                <div class="form-group">
-                    <label for="exampleInputEmail1" class="form-label mt-4">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"></input>
-                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-                </div>
-                <div class="form-group">
-                    <label for="exampleInputPassword1" class="form-label mt-4">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"></input>
-                </div>
-                <button type="submit" class="btn btn-primary">Sign in</button>
-                <button type="button" class="btn btn-outline-secondary">Registration</button>
-                </fieldset>
-            </form>
-        </div>
-            <Footer />
+            <div class="row justify-content-center align-items-center" id='sign' >
+                <form class="sign-in">
+                    <fieldset>
+                    <div class="form-group">
+                        <label for="exampleInputEmail1" class="form-label mt-4">Email address</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"></input>
+                        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1" class="form-label mt-4">Password</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"></input>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Sign in</button>
+                    <button type="button" class="btn btn-outline-secondary">Registration</button>
+                    </fieldset>
+                </form>
+            </div>
+            <div class="fixed-bottom">
+                <Footer />
+            </div>
         </>
     )
 }
