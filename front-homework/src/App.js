c1d4ea41cbabcb9929b8d8dc2b8e5c255750acd7
Roman Riabenko <beatle.gaze@gmail.com>
2022-11-01 20:20:46 +0200

    Configure App.js for testing

--- front-homework/src/App.js
+++ front-homework/src/App.js
@@ -4,12 +4,13 @@ import './css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.js';
 
 import MainPage from './pages/mainPage/MainPage';
-import SignInPage from './pages/mainPage/SignInPage';
+import SignInPage from './pages/signinPage/SignInPage';
+import PaginationPage from './pages/paginationPage/PaginationPage';
 
 function App() {
   return (
     <div className="App">
-       <SignInPage/>
+       <PaginationPage/>
     </div>
   );
 }
