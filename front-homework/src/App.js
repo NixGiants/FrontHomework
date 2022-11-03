6c0d4c5c0389031f8e41a8a4bd09019d88f26731
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-11-03 12:31:59 +0300

    Add routing

--- front-homework/src/App.js
+++ front-homework/src/App.js
@@ -2,15 +2,25 @@ import React from 'react';
 
 import './css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.js';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import MainPage from './pages/mainPage/MainPage';
 import SignInPage from './pages/signinPage/SignInPage';
 import PaginationPage from './pages/paginationPage/PaginationPage';
 
+
 function App() {
   return (
     <div className="App">
-       <PaginationPage/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/Login" element={<SignInPage />} />
+          <Route path="/MainPage" element={<MainPage />} />
+          <Route path="/Pagination" element={<PaginationPage />} />
+            /* Default Router */
+          <Route path="/" element={<Navigate to="/Login" />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
